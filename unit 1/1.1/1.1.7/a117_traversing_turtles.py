5a81d9865a2f14ad5637bbf61a6f68d2cdbb1ec9
tejas <tcherukuru165@g.fhsdschools.org>
2024-10-10 07:25:45 -0500

    finished 1.1.7

--- unit 1/1.1/1.1.7/a117_traversing_turtles.py
+++ unit 1/1.1/1.1.7/a117_traversing_turtles.py
@@ -4,31 +4,40 @@ import turtle as trtl
 
 # create an empty list of turtles
 my_turtles = []
-
+direction = 90
 
 # use interesting shapes and colors
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
-  t = trtl.Turtle(shape=s)
-  my_turtles.append(t)
+		t = trtl.Turtle(shape=s)
+		my_turtles.append(t)
 
 #  defines where the turtles will start (x and y coordinates)
 startx = 0
 starty = 0
 
-# for loop to move the turtles to each new location 
+# for loop to move the turtles to each new location
 for t in my_turtles:
-  t.penup()
-  t.goto(startx, starty)
-  t.pendown()
-  t.right(45)     
-  t.forward(50)
+		new_color = turtle_colors.pop()
+		t.fillcolor(new_color)
+		t.pencolor(new_color)
+		t.penup()
+		t.goto(startx, starty)
+		t.pendown()
+		t.setheading(direction)
+		t.right(45)
+		t.forward(50)
 
-# moves where the turtle starts to create new places for shapes by chanign start x and y
-  startx = startx + 50
-  starty = starty + 50
+		# moves where the turtle starts to create new places for shapes by chanign start x and y
+		startx = startx + 50
+		starty = starty + 50
+
+		startx = t.xcor()
+		starty = t.ycor()
+  
+		direction = t.heading()
 
 wn = trtl.Screen()
 wn.mainloop()
